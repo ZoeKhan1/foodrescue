877ee7a8d077dad2d66c995877ca9e4a779b1db7
ZoeKhan1 <zkhan@smith.edu>
2025-08-01 22:42:52 -0700

    Document raw food

---
+++ R/data_document.R
@@ -0,0 +1,9 @@
+#' raw_food: raw data from the Food Rescue Network Qualtrics survey
+#' Contains information regarding dining halls, food type, and weight.
+#'
+#' @format A data frame with 121 rows and 248 variables:
+#'
+#' Columns correspond to different Qualtrics questions.
+#' Use `names(raw_food)` to learn more.
+#'
+"raw_food"
