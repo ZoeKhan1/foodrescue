ea9cb2879835f1622a6e787c1e70978f12b644dc
ZoeKhan1 <zkhan@smith.edu>
2025-08-03 02:05:25 -0700

    Pull corrected Qualtrics data and incorporate

--- R/data_document.R
+++ R/data_document.R
@@ -1,7 +1,7 @@
 #' raw_food: raw data from the Food Rescue Network Qualtrics survey
 #' Contains information regarding dining halls, food type, and weight.
 #'
-#' @format A data frame with 121 rows and 248 variables:
+#' @format A data frame with 238 rows and 248 variables:
 #'
 #' Columns correspond to different Qualtrics questions.
 #' Use `names(raw_food)` to learn more.
